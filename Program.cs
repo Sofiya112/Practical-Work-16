d71cc2e4762673a2aec5311e2191cef19e20785e
Sofiya112 <sofiya032005@gmail.com>
2024-12-16 21:56:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            ArrayList collection = new ArrayList();
+            collection.Add(24);
+            collection.Add("Hello, World!");
+            collection.Add(true);
+            collection.Add(1.16);
+            Output(collection);
+            collection.RemoveAT(2);
+
+        }
+        static void Output(ArrayList collection)
+        {
+            foreach (var el in collection)
+            {
+                Console.WriteLine(el);
+            }
+            {
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+
+    }
+
